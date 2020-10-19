a395340c277d70f6e91a72efd9ff7f9eee93708d
Victor Anaya <victorgotzilla@hotmail.com>
2020-10-19 18:36:07 -0500

    function srand added

--- acr/matrix.c
+++ acr/matrix.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int main(int argc, char *argv[]){
-    int filas_A,colum_A, filas_B, colum_B;
+    srand(time(NULL));
+
+    int filas_A,colum_A, filas_B, colum_B,numero_hilos;
     printf("ingresa las filas de la matriz A\n");
     scanf("%d",&filas_A);
     printf("ingresa las columnas de la matriz A\n");
     scanf("%d",&colum_A);
-    printf("NOTA: para que la multiplicacion sea valida las filas de B deben ser las mismas que las columnas de A\n");
-    printf("por lo que el valor ingresado en columnas de A sera el mismo valor que las filas de B\n");
+    printf("\nNOTA: para que la multiplicacion sea valida las filas de B deben ser las mismas que las columnas de A\n");
+    printf("por lo que el valor ingresado en columnas de A sera el mismo valor que las filas de B\n\n");
     filas_B = colum_A;
     printf("ingresa las columnas de la matriz B\n");
     scanf("%d",&colum_B);
@@ -17,6 +20,13 @@ int main(int argc, char *argv[]){
     printf("columnas de A: %d\n",colum_A);
     printf("filas de B: %d\n",filas_B);
     printf("columnas de B: %d\n",colum_B);
+    printf("Cuantos hilos quieres usar? NOTA el numero de hilos debe ser menor o igual que las filas de A\nNumero recomendado de hilos:%d\n",filas_A);
+    scanf("%d",&numero_hilos);
+    while(numero_hilos > filas_A){
+        printf("porfavor, el numero de hilos debe ser menor o igual que la cantidad de filas de A\nNumero recomendado:%d\n",filas_A);
+        scanf("%d",&numero_hilos);
+    }
+    
     
     //llenamos las matricez con valores aleatorios
     int matrixA[filas_A][colum_A];
@@ -24,8 +34,8 @@ int main(int argc, char *argv[]){
 
     for (int i = 0; i < filas_A; i++){
         for (int j = 0; j < colum_A; j++){
-            printf("digite el numero en la posicion %d %d  ",i,j);
-            scanf("%d",&matrixA[i][j]);
+            matrixA[i][j] = rand()%6;
+            //scanf("%d",&matrixA[i][j]);
         }
     }
     for (int i = 0; i < filas_A; i++){
@@ -33,7 +43,7 @@ int main(int argc, char *argv[]){
         for (int j = 0; j < colum_A; j++){
             printf("%d",matrixA[i][j]);
         }
-        
     }
+    printf("\n");
     return 0;
 }
