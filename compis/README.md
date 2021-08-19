a2f3c8c6c95fdef1588af1da17700e9eb7040934
victorA <victorgotzilla@hotmail.com>
2021-08-18 23:03:19 -0700

    compis dir added to ESCOM-dir

---
+++ compis/README.md
@@ -0,0 +1,3 @@
+# ESCOM notas de mi estancia en ESCOM
+
+Update: 9no semestre ESCOM
---
+++ compis/clases.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>>
+
+using namespace std;
+
+class Persona{
+    private:    //atributos
+        int age;
+        string name;
+    public:     //metodos
+        Persona(int, string);//Constructor
+        void leer();
+        void correr();
+};
+
+//Constructor
+Persona::Persona(int _age, string _name){
+    age =_age;
+    name=_name;
+}
+
+void Persona::leer(){
+    cout<<"soy "<< name << " y estoy leyendo"<<endl;
+}
+
+void Persona::correr(){
+    cout<<"soy "<< name << " y estoy corriendo"<<endl;
+}
