275e64b1959f79b95d6b9595528324304cb52481
victorA <victorgotzilla@hotmail.com>
2021-10-03 16:16:34 -0700

    fibo modificado

--- AnalisisDeAlgoritmos/fibonacci.c
+++ AnalisisDeAlgoritmos/fibonacci.c
@@ -5,33 +5,17 @@
 int main(int argc, char *argv[]){
     int a=0;
     int b=1;
-    int c;
+    int c=0;
     int numero_max;
     printf("Ingresa un numero entero\n");
     scanf("%d",&numero_max);
-    switch (numero_max){
-    case 0:
-        printf("la serie fibo es: 0\n");
-        break;
-    case 1:
-        printf("la serie fibo es: 0,1\n");
-        break;
-    default:
-        printf("la serie fibo es: 0, 1, ");
-        do{
-            c=a+b;
-            if (c > numero_max){
-                break;
-            }
-        
-            printf("%d, ",c);
-            a=b;
-            b=c;
-        } while (c <= numero_max);
-        printf("\n");
-        break;
-    }
-    
-    
-    
+    do{
+        printf("%d, ",c);
+        c=a+b;
+        if (c > numero_max){
+            break;
+        }
+        a=b;
+        b=c;
+    } while (c <= numero_max);    
 }
