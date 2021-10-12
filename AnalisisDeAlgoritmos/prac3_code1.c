69f1109e2576ad65ae9a7e254c189868a41b82cc
victorA <victorgotzilla@hotmail.com>
2021-10-12 14:43:44 -0700

    contadores added

--- AnalisisDeAlgoritmos/prac3_code1.c
+++ AnalisisDeAlgoritmos/prac3_code1.c
@@ -9,48 +9,66 @@
 */
 
 int division1(int nEntrada, int divEntrada){
+    int contador=5;
     int n=nEntrada;
     int div=divEntrada;
     int res=0;
     int q = 0;
     printf("%d",n);
     while(n>=div){
-        n=n-div; 
-        q=q+1;
+        contador++;
+        contador++;
+        n=n-div; contador++;
+        q=q+1; contador++;
     }
-    res=n;
-    printf(" = %d * %d + %d\n",div,q,res);
+    res=n; contador++;
+    printf(" = %d * %d + %d\n",div,q,res);contador++;
+    contador++;
+    printf("pasos: %d\n",contador);
     return q;
 }
 
 int division2(int nEntrada, int divEntrada){
+    int contador=4;
     int dd = divEntrada;
     int q=0;
     int r = nEntrada;
     
     printf("%d",nEntrada);
     while(dd<=nEntrada){
-        dd=2*dd;
+        contador++;
+        contador++;
+        dd=2*dd; contador++;
     }
     while(dd>divEntrada){
-        dd=dd/2;
-        q=2*q;
+        contador++;
+        contador++;
+        dd=dd/2; contador++;
+        q=2*q; contador++;
+        contador++;
         if (dd<=r){
-            r=r-dd;
-            q=q+1;
+            contador++;
+            r=r-dd; contador++;
+            q=q+1; contador++;
         }        
     }
     printf(" = %d * %d + %d\n",dd,q,r);
+    printf("pasos: %d\n",contador);
     return q;
 }
 
+int contadorDiv3=0;
 int division3(int nEntrada, int divEntrada){
+    contadorDiv3++;
     if (divEntrada>nEntrada){
+        contadorDiv3++;
+        printf("pasos: %d\n",contadorDiv3);
         return 0;
     }else{
+        contadorDiv3++;
+        contadorDiv3++;
         return 1+division3(nEntrada-divEntrada,divEntrada);
     }
-    
 }
 
 int main(int argc, char *argv[]){
